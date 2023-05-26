78be8b897ed16f58e565dc3b1fa289c859e3a652
Thais Bueno <tha.bueno.tbp@gmail.com>
2023-05-25 21:40:36 -0300

    ultimas alteracoes

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("o exercício foi iniciado")
+// criação de sript deu certo, o que precisa ser feto para rodar é node index.js start || npm run start (ou npm start), que fica igual do React, mas não é react e todos esses comandos executam, esse console.log
+// Existe também o jeito do exercício 2 pra rodar o que esta dentro do arquivo node exercicios/process-argv.js
